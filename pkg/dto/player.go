e0691e419a69a49b08187a7ac8350b6058ef1730
Alex <prestonalexwebster@gmail.com>
2026-01-19 03:06:13 +0300

    TTS v2 + GOT characters

--- pkg/dto/player.go
+++ pkg/dto/player.go
@@ -1,12 +1,15 @@
 package dto
 
 type PlayerV2 struct {
-	ID       int
-	Name     string
-	Model    string
-	Role     string
-	Voice    string
-	Mood     string
-	Position int
-	GameID   int
+	ID               int
+	Name             string
+	Model            string
+	Role             string
+	Voice            string
+	Mood             string
+	TtsModel         string
+	VoiceTemperature float32
+	VoiceStyle       string
+	Position         int
+	GameID           int
 }
